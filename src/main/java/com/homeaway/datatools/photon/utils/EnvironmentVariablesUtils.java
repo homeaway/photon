375ffb8553b32fc11e071a1481ad2638380fb2fe
Andrei Luksha2 <andrei_luksha2@epam.com>
2022-05-26 13:51:45 +0600

    EGD-5267: read default processingLoopInterval for DefaultAsyncMessageProcessor from DEFAULT_MESSAGE_PROCESSOR_LOOP_INTERVAL_MS environment variable

---
+++ src/main/java/com/homeaway/datatools/photon/utils/EnvironmentVariablesUtils.java
@@ -0,0 +1,55 @@
+package com.homeaway.datatools.photon.utils;
+
+import org.slf4j.Logger;
+
+import java.util.function.Function;
+
+public class EnvironmentVariablesUtils {
+    public static <T> T getEnvVarOrUseDefault(
+            final String envVarName,
+            T defaultValue,
+            Logger log,
+            Function<String, T> parsingFunction
+    ) {
+        T result;
+        final String envVarValue = System.getenv(envVarName);
+        if (envVarValue == null) {
+            log.info(
+                    "Environment variable {} is not set. Will use the default {} ms value.",
+                    envVarName,
+                    defaultValue
+            );
+            result = defaultValue;
+        } else {
+            log.info(
+                    "ProcessingLoopInterval value read from {} environment variable is {} ms.",
+                    envVarName,
+                    envVarValue
+            );
+            result = parsingFunction.apply(envVarValue);
+        }
+        return result;
+    }
+
+    public static long getLongOrUseDefault(final String envVarName, Long defaultValue, Logger log) {
+        Function<String, Long> parsingFunction = (String arg) -> parseLong(envVarName, arg);
+        return getEnvVarOrUseDefault(envVarName, defaultValue, log, parsingFunction);
+    }
+
+    public static long parseLong(final String envVarName, final String envVarValue) {
+        long result;
+        try {
+            result = Long.parseLong(envVarValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Failed to parse value %s of environment variable %s as a Long value.",
+                            envVarValue,
+                            envVarName
+                    ),
+                    e
+            );
+        }
+        return result;
+    }
+}
